e7fe614edbdce5d619ce06de1a49198f015ee2da
ShamanEstBanan <kjldfgjsdkflg>
2019-11-23 19:07:52 +0300

    1 comm

--- src/ru/kalkshtein/unicorn/App.java
+++ src/ru/kalkshtein/unicorn/App.java
@@ -1,7 +1,24 @@
 package ru.kalkshtein.unicorn;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello worlds!");
+        BrunoQueue myQueue = new BrunoQueue();
+        myQueue.add(1);
+        myQueue.add(2);
+        myQueue.add(3);
+
+        System.out.println(myQueue.get());
+        System.out.println(myQueue.get());
+        System.out.println(myQueue.get());
+        System.out.println(myQueue.get());
+        System.out.println(myQueue.get());
+
     }
+
 }
